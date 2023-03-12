fb454994186232a7ad3d16c312c36dcb952912a3
Timothy Dam <vietdam89@gmail.com>
2023-03-11 20:25:25 -0500

    lab complete

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+const names =["Guadalupe", "Ollie","Aki"];
+const messages = [];
+function writeCards(names, eventNames){
+       for (let i = 0; i < names.length; i ++)
+       { let message = `Thank you, ${names[i]}, for the wonderful ${eventNames} gift!`;
+          messages.push(message);
+        } 
+        return messages;
+    }
+function countDown(number){
+       while (number >= 0){
+           console.log(number);
+                  number --;
+       }
+    }
